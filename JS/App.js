3de0fe3c6b6615b8fc4c1ffa2eeea61a244a45a4
ajinkya6373 <72693966+ajinkya6373@users.noreply.github.com>
2021-04-16 18:31:12 +0530

    Update App.js

--- JS/App.js
+++ JS/App.js
@@ -1,30 +1,23 @@
 
 let image = document.getElementById("image");
-// profile.innerHTML = `<img src="img/aj-1.jpg" alt=""></img>`
-
 let currentPos = 0;
 let images = ["/img/aj-1.jpg", "/img/aj-2.jpg", "/img/aj-3.jpg", "/img/aj-6.jpg", "/img/aj-7.jpg", "/img/aj-4.jpg"]
 
 function changeImage() {
     if (++currentPos >= images.length)
         currentPos = 0;
-    // console.log(currentPos)
     image.src = images[currentPos];
 }
 
-setInterval(changeImage, 6000);
+image.addEventListener("click",  changeImage)
+// setInterval(changeImage, 6000);
 
 function getData() {
     let proCard = document.getElementById("proCard");
     url = "data.json"
     fetch(url).then((response) => {
-
         return response.json();
-
     }).then((data) => {
-        // console.log(data);
-        // console.log(data[0].title);
-        // console.log(data[1].title);
         for (let i = 0; i < data.length; i++) {
             let obj = data[i];
             // console.log(obj.title);
